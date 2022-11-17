f1449ac08780cf0d07ed4a312ef55aa20fc73c27
Jordan <jordan.krp.93@gmail.com>
2022-11-17 13:04:30 +0100

    Added stories app

--- photography/settings.py
+++ photography/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'trips.apps.TripsConfig',
-    'picture.apps.PictureConfig',
+    'stories.apps.StoriesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
