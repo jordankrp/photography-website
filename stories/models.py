bd838c2dc4e8f0ab396bcddf91d441a627eee134
Jordan <jordan.krp.93@gmail.com>
2022-11-17 13:46:39 +0100

    Building stories app and model

--- stories/models.py
+++ stories/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Story(models.Model):
+    title = models.CharField(max_length=255)
+    story_image = models.ImageField(upload_to='story_images/')
+    summary = models.CharField(max_length=2000)
