661ccfaa42d618efe6a6606ef00416b2c31d906a
Jordan <jordan.krp.93@gmail.com>
2022-11-18 13:18:00 +0100

    stories html wip...

--- stories/views.py
+++ stories/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Story
 
-# Create your views here.
+def allstories(request):
+    stories = Story.objects
+    return render(request, 'stories/allstories.html', {'stories':stories})
