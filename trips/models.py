aa557975ff933486e556c696262203c706c466d8
Jordan <jordan.krp.93@gmail.com>
2022-11-21 21:32:55 +0100

    Modifying tips template

--- trips/models.py
+++ trips/models.py
@@ -4,3 +4,6 @@ class Trip(models.Model):
     title = models.CharField(max_length=255)
     cover_image = models.ImageField(upload_to='cover_images/')
     summary = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
