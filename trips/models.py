11c3a1abea462120b98f858815d9270c25e55342
Jordan <jordan.krp.93@gmail.com>
2022-11-27 16:48:25 +0100

    Display 9 photos per trip

--- trips/models.py
+++ trips/models.py
@@ -4,6 +4,15 @@ class Trip(models.Model):
     title = models.CharField(max_length=255)
     cover_image = models.ImageField(upload_to='cover_images/')
     summary = models.CharField(max_length=200)
+    photo1 = models.ImageField(upload_to='trip_images/')
+    photo2 = models.ImageField(upload_to='trip_images/')
+    photo3 = models.ImageField(upload_to='trip_images/')
+    photo4 = models.ImageField(upload_to='trip_images/')
+    photo5 = models.ImageField(upload_to='trip_images/')
+    photo6 = models.ImageField(upload_to='trip_images/')
+    photo7 = models.ImageField(upload_to='trip_images/')
+    photo8 = models.ImageField(upload_to='trip_images/')
+    photo9 = models.ImageField(upload_to='trip_images/')
 
     def __str__(self):
         return self.title
